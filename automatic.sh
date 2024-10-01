c9f91e7feea1a75f2dd7789ac0a363e81a949933
BiGnotes <74706496+BiGnotes@users.noreply.github.com>
2024-10-02 00:09:42 +0800

    Delete automatic.sh

--- automatic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# 关闭https
-sed -i "/list listen_https '0.0.0.0:443'/d" package/network/services/uhttpd/files/uhttpd.config
-sed -i "/list listen_https '[::]:443'/d" package/network/services/uhttpd/files/uhttpd.config
