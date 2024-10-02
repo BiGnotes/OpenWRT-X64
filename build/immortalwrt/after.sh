c7347de6bc4c9edb45c055dcf47e355165fe11f9
BiGnotes <74706496+BiGnotes@users.noreply.github.com>
2024-10-02 23:55:58 +0800

    Update after.sh

--- build/immortalwrt/after.sh
+++ build/immortalwrt/after.sh
@@ -30,10 +30,26 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 sed -i "s#/bin/login#/bin/login -f root#g" feeds/packages/utils/ttyd/files/ttyd.config
 
 ##自定义luci程序
+# netdata
+sed -i 's/system/status/g;s/10/90/g' feeds/luci/applications/luci-app-netdata/luasrc/controller/netdata.lua
 
+# 解锁云音乐
+sed -i 's/50/55/g;s/解除网易云音乐播放限制/解锁云音乐/g' feeds/luci/applications/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
+
+
+# FTP
+sed -i 's/_("FTP Server")/_("FTP Server"), 5/' feeds/luci/applications/luci-app-vsftpd/luasrc/controller/vsftpd.lua
+
+# Zerotier
+sed -i 's/vpn/nas/g;s/90/30/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
 
 
 ##修改菜单名称
+#MultiWAN 管理器改成负载均衡
+#sed -i 's/MultiWAN 管理器/负载均衡/g' `grep "MultiWAN 管理器" -rl ./feeds/luci/`
+
+#UPnP IGD 和 PCP/NAT-PMP改成UPnP
+#sed -i 's/UPnP IGD 和 PCP/NAT-PMP/UPnP/g' `grep "UPnP IGD 和 PCP/NAT-PMP" -rl ./feeds/luci/`
 
 #带宽监控改成监控
 #sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./feeds/luci/`
